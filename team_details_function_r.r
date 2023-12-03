c834a94bf3176d4ba63860fa20211be726f15f2c
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2023-12-03 23:03:54 +0000

    Update team_details_function_r.r
    Filling in NAs caused by team being an invitational team.

--- team_details_function_r.r
+++ team_details_function_r.r
@@ -2,5 +2,7 @@ library(tidyverse)
 
 team_details_function <- function() {
   team_details_csv <- read.csv('https://raw.githubusercontent.com/rogers1000/cyclingchaos/main/CyclingChaos_team_details.csv') |>
-  mutate(team_id = as.character(team_id))
+  mutate(team_id = as.character(team_id)) |>
+  mutate(team_name = ifelse(team_id == 'None',team_name_invitational,team_name)) |>
+  mutate(uci_division = replace_na(uci_division,'Invitational Team'))
 }
