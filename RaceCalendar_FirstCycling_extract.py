9e5b8c644f705b8462bbffedb8b9de9b0e9fd97e
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2023-12-03 12:43:41 +0000

    Update RaceCalendar_FirstCycling_extract.py
    commenting out google colab modules

--- RaceCalendar_FirstCycling_extract.py
+++ RaceCalendar_FirstCycling_extract.py
@@ -3,20 +3,20 @@
 import pandas as pd
 import numpy as np
 import requests
-from google.colab import data_table
+# from google.colab import data_table
 from datetime import datetime
 import time
 from datetime import timedelta
 from tqdm import tqdm
 from bs4 import BeautifulSoup
-from google.colab import files
+# from google.colab import files
 import os
 import re
 from time import sleep
-from google.colab import drive
+# from google.colab import drive
 from random import randrange
-data_table.enable_dataframe_formatter()
-drive.mount('drive')
+# data_table.enable_dataframe_formatter()
+# drive.mount('drive')
 
 ### Men's Road Calendar ###
 
