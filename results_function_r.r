9d6253e6bc26da5c35590099ea30e1bfe77138b6
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2023-12-03 23:01:37 +0000

    Update results_function_r.r
    Making ids varchar rather than numeric.

--- results_function_r.r
+++ results_function_r.r
@@ -4,7 +4,9 @@ results_function <- function() {
   results_csv <- read.csv('https://raw.githubusercontent.com/rogers1000/cyclingchaos/main/CyclingChaos_RaceResults_2023.csv') |>
     left_join(team_details <- team_details_function(), by = c('season','team_id')) |>
     mutate(team_name = ifelse(team_id == 'None',team_name_invitational,team_name)) |>
-    mutate(uci_division = replace_na(uci_division,'Invitational Team')) |>
-    select(-c(team_name_invitational,uci_division,race_nationality)) |>
-    relocate(team_name,.after = team_id)
+    select(-c(team_name_invitational,uci_division,race_nationality,gc_position,X,gc_time,race_tag)) |>
+    relocate(team_name,.after = team_id) |>
+    mutate(rider_id = as.character(rider_id)) |>
+    mutate(team_id = as.character(team_id)) |>
+    mutate(race_id = as.character(race_id))
 }
