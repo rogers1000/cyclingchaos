a814de2d9e7f32e212592890648db4ac456c72f2
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2023-12-06 18:54:59 +0000

    Create raceresults_stages_extract.py
    Uploading extract to get race_results stage race stages from FirstCycling.com

---
+++ backend_race_results/raceresults_stages_extract.py
@@ -0,0 +1,17 @@
+### Bring in Modules
+
+import pandas as pd
+import numpy as np
+import requests
+# from google.colab import data_table
+from datetime import datetime
+import time
+from datetime import timedelta
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+# from google.colab import files
+import os
+import re
+from time import sleep
+from random import randrange
+# data_table.enable_dataframe_formatter()
