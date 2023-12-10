ce04a8aa25c09fe8fdcbb927b439b9ba4de52a45
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2023-12-10 00:05:36 +0000

    Update team_details_backend_function_r.r
    updated `team_id` to `first_cycling_team_id`

--- backend_team_details/team_details_backend_function_r.r
+++ backend_team_details/team_details_backend_function_r.r
@@ -2,7 +2,7 @@ library(tidyverse)
 
 team_details_function <- function() {
   team_details_csv <- read.csv('https://raw.githubusercontent.com/rogers1000/cyclingchaos/main/CyclingChaos_team_details.csv') |>
-  mutate(team_id = as.character(team_id)) |>
+  mutate(first_cycling_team_id = as.character(first_cycling_team_id)) |>
   mutate(team_name = ifelse(team_id == 'None',team_name_invitational,team_name)) |>
   mutate(uci_division = replace_na(uci_division,'Invitational Team'))
 }
