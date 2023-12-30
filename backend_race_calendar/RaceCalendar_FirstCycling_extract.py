0e1f79b42e79f116ea103763d481bb637ed282a5
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2023-12-30 15:41:27 +0000

    Update RaceCalendar_FirstCycling_extract.py
    Phase 5 Race Calendar Extract, Write to Disk Methodology.

--- backend_race_calendar/RaceCalendar_FirstCycling_extract.py
+++ backend_race_calendar/RaceCalendar_FirstCycling_extract.py
@@ -1,105 +1,75 @@
-### Importing Packages ###
-
+# Importing Modules required for code to work
 import pandas as pd
 import numpy as np
-import requests
-# from google.colab import data_table
 from datetime import datetime
+import requests
 import time
 from datetime import timedelta
 from tqdm import tqdm
 from bs4 import BeautifulSoup
-# from google.colab import files
 import os
 import re
 from time import sleep
-# from google.colab import drive
 from random import randrange
-# data_table.enable_dataframe_formatter()
-# drive.mount('drive')
 
-### Men's Road Calendar ###
+##### Making Code Possible to Put onto Github #####
+
+#setwd = #Your Working Directory
+
+##### Men's Calendar Ingestion #####
+cci_output = []
+cci_output_details = []
+cci_file_name = []
 
 month = 0
-# Change depending on Season
+month_str = np.where(month < 10,'0'+str(month),str(month))
 season = 2023
 
 month_extract = 0
 
-month_str = np.where(month < 10,'0'+str(month),str(month))
-
-current_month = datetime.now().month
-
-current_month_range_adj = np.where(current_month < 12,current_month,12)
-
-mens_road_calendar_df = pd.DataFrame(columns=['season','gender','first_cycling_race_id','race_name','category','race_nationality','uci_race_classification','stage_race_boolean','start_date','end_date'])
-
-for month_extract in tqdm(range(1,
-                                # 3
+for month_extract in tqdm(range(0,
+                                #2
                                 12
                                 )):
   season = np.where(month == 12,int(season)+1,season)
   month = np.where(month < 12,month + 1,1)
+  # month = 1
   month_str = np.where(month < 10,'0'+str(month),str(month))
   time.sleep(5)
   url = 'https://firstcycling.com/race.php?y='+str(season)+'&t=2&m='+str(month_str)
   calendar_mens_road_df_meta = requests.get(url)
   calendar_mens_road_df_meta_soup = BeautifulSoup(calendar_mens_road_df_meta.content, "html.parser")
-  calendar_mens_road_df_meta_soup_part2 = calendar_mens_road_df_meta_soup.find_all('tbody')[-1]
+  calendar_mens_road_df_meta_soup_str = str(calendar_mens_road_df_meta_soup)
+  file_name = 'cycling_chaos_code'+'_'+'calendar'+'_'+'men'+'_'+str(season)+'_'+str(month_str)+'.txt
+#   with open(r'/Users/zacrogers/Documents/cycling_chaos/python_code/calendar_ingestion_files/'+file_name, 'w') as writefile:
+  with open(setwd+'calendar_ingestion_files/'+file_name, 'w') as writefile:
+    writefile.write(calendar_mens_road_df_meta_soup_str)
+    writefile.close()
+  cci_output.append('calendar')
+  cci_output_details.append('Men')
+  cci_file_name.append(file_name)
 
-  for row in calendar_mens_road_df_meta_soup_part2.find_all('tr'):
-    columns = row.find_all('td')
+  print('Ingested '+file_name)
 
-    if(columns != []):
-        season = str(season)
-        gender = 'Men'
-        category = 'Road'
-        first_cycling_race_id = str(columns[2].find_all('a')).split('r=')[1].split('&amp')[0]
-        race_name = str(columns[2].find_all('a')).split('title="')[1].split('">')[0]
-        race_nationality = str(columns[2].find_all("span")).split("flag-")[1].split('"><')[0].upper()
-        uci_race_classification = columns[1].text
-        stage_race_boolean = np.where(columns[1].text.startswith('2'),"Stage Race","One Day")
-        start_date = str(season)+'/'+columns[0].text.split('-')[0].split('.')[-1]+'/'+columns[0].text.split('-')[0].split('.')[0]
-        end_date = str(season)+'/'+columns[0].text.split('-')[-1].split('.')[-1]+'/'+columns[0].text.split('-')[-1].split('.')[0]
-        # test5 = columns[5]
+cycling_chaos_ingestion = pd.DataFrame({'output':cci_output, 'output_details':cci_output_details, 'file_name':cci_file_name})
 
-        mens_road_calendar_df = mens_road_calendar_df.append({
-            'season':season
-            ,'gender':gender
-            ,'category':category
-            ,'first_cycling_race_id':first_cycling_race_id
-            ,'race_name':race_name
-            ,'race_nationality':race_nationality
-            ,'uci_race_classification':uci_race_classification
-            ,'stage_race_boolean':stage_race_boolean
-            ,'start_date':start_date
-            ,'end_date':end_date
-            # ,'test5':test5
-            # ,'test0':test0
-        }, ignore_index=True)
+cycling_chaos_ingestion.to_csv(setwd+'cycling_chaos_ingestion_df.csv', index=False)
 
-### Women's Road Calendar ###
+print(cycling_chaos_ingestion)
+
+##### Women's Calendar Ingestion #####
+cycling_chaos_ingestion = pd.read_csv('/Users/zacrogers/Documents/cycling_chaos/python_code/cycling_chaos_ingestion_df.csv')
 
 month = 0
-
-# Change depending on Season extracting
+month_str = np.where(month < 10,'0'+str(month),str(month))
 season = 2023
 
 month_extract = 0
 
-month_str = np.where(month < 10,'0'+str(month),str(month))
-
-current_month = datetime.now().month
-
-current_month_range_adj = np.where(current_month < 12,current_month,12)
-
-womens_road_calendar_df = pd.DataFrame(columns=['season','gender','first_cycling_race_id','race_name','category','race_nationality','uci_race_classification','stage_race_boolean','start_date','end_date'])
-
-for month_extract in tqdm(range(1,
-                                # 3
-                                10
+for month_extract in tqdm(range(0,
+                                #2
+                                12
                                 )):
-  # sleep_time = randrange(4.5,5.5)
   season = np.where(month == 12,int(season)+1,season)
   month = np.where(month < 12,month + 1,1)
   # month = 1
@@ -108,44 +78,20 @@ for month_extract in tqdm(range(1,
   url = 'https://firstcycling.com/race.php?y='+str(season)+'&t=6&m='+str(month_str)
   calendar_womens_road_df_meta = requests.get(url)
   calendar_womens_road_df_meta_soup = BeautifulSoup(calendar_womens_road_df_meta.content, "html.parser")
-  calendar_womens_road_df_meta_soup_part2 = calendar_womens_road_df_meta_soup.find_all('tbody')[-1]
+  calendar_womens_road_df_meta_soup_str = str(calendar_womens_road_df_meta_soup)
+  file_name = 'cycling_chaos_code'+'_'+'calendar'+'_'+'women'+'_'+str(season)+'_'+str(month_str)+'.txt'
+  # with open(r'/Users/zacrogers/Documents/cycling_chaos/python_code/calendar_ingestion_files/'+file_name, 'w') as writefile:
+  with open(setwd+'calendar_ingestion_files/'+file_name, 'w') as writefile:
+    writefile.write(calendar_womens_road_df_meta_soup_str)
+    writefile.close()
+    cci_output.append('calendar')
+    cci_output_details.append('Women')
+    cci_file_name.append(file_name)
 
-  for row in calendar_womens_road_df_meta_soup_part2.find_all('tr'):
-    columns = row.find_all('td')
+  print('Ingested '+file_name)
 
-    if(columns != []):
-        season = str(season)
-        gender = 'Women'
-        category = 'Road'
-        first_cycling_race_id = str(columns[2].find_all('a')).split('r=')[1].split('&amp')[0]
-        race_name = str(columns[2].find_all('a')).split('title="')[1].split('">')[0]
-        race_nationality = str(columns[2].find_all("span")).split("flag-")[1].split('"><')[0].upper()
-        uci_race_classification = columns[1].text
-        stage_race_boolean = np.where(columns[1].text.startswith('2'),"Stage Race","One Day")
-        start_date = str(season)+'/'+columns[0].text.split('-')[0].split('.')[-1]+'/'+columns[0].text.split('-')[0].split('.')[0]
-        end_date = str(season)+'/'+columns[0].text.split('-')[-1].split('.')[-1]+'/'+columns[0].text.split('-')[-1].split('.')[0]
-        # test5 = columns[5]
+cycling_chaos_ingestion = pd.DataFrame({'output':cci_output, 'output_details':cci_output_details, 'file_name':cci_file_name})
 
-        womens_road_calendar_df = womens_road_calendar_df.append({
-            'season':season
-            ,'gender':gender
-            ,'category':category
-            ,'first_cycling_race_id':first_cycling_race_id
-            ,'race_name':race_name
-            ,'race_nationality':race_nationality
-            ,'uci_race_classification':uci_race_classification
-            ,'stage_race_boolean':stage_race_boolean
-            ,'start_date':start_date
-            ,'end_date':end_date
-            # ,'test5':test5
-            # ,'test0':test0
-        }, ignore_index=True)
+cycling_chaos_ingestion.to_csv(setwd+'cycling_chaos_ingestion_df.csv', index=False)
 
-### Putting final steps of calendar together ###
-
-calendar_df = pd.concat([mens_road_calendar_df,womens_road_calendar_df], ignore_index = True)
-
-calendar_df = calendar_df.drop_duplicates(subset = 'first_cycling_race_id')
-
-
-
+print(cycling_chaos_ingestion)
