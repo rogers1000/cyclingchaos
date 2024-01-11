1c36f02598d700c507ad1e6aa752c6d7aa10d970
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2024-01-11 23:01:39 +0000

    Update team_details_backend_function_r.r
    updated since new transformation changed a few things.

--- backend_team_details/team_details_backend_function_r.r
+++ backend_team_details/team_details_backend_function_r.r
@@ -1,8 +1,7 @@
 library(tidyverse)
 
 team_details_function <- function() {
-  team_details_csv <- read.csv('https://raw.githubusercontent.com/rogers1000/cyclingchaos/main/CyclingChaos_team_details.csv') |>
-  mutate(first_cycling_team_id = as.character(first_cycling_team_id)) |>
+  team_details_csv <- read.csv('https://raw.githubusercontent.com/rogers1000/cyclingchaos/main/backend_team_details/CyclingChaos_backend_team_details.csv') |>
   mutate(team_name = ifelse(first_cycling_team_id == 'None',team_name_invitational,team_name)) |>
-  mutate(uci_division = replace_na(uci_division,'Invitational Team'))
+  mutate(uci_division_name = replace_na(uci_division_name,'Invitational Team'))
 }
