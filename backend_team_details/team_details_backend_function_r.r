caaaec010cf60ac34f7f0c78c94bbf5f75c3a313
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2024-01-26 22:53:53 +0000

    Update team_details_backend_function_r.r
    added commentary

--- backend_team_details/team_details_backend_function_r.r
+++ backend_team_details/team_details_backend_function_r.r
@@ -1,7 +1,11 @@
 library(tidyverse)
 
 team_details_function <- function() {
+  # load in csv
   team_details_csv <- read.csv('https://raw.githubusercontent.com/rogers1000/cyclingchaos/main/backend_team_details/CyclingChaos_backend_team_details.csv') |>
+  # if no team name then use invitational team name
   mutate(team_name = ifelse(first_cycling_team_id == 'None',team_name_invitational,team_name)) |>
+  # if no uci_division then put as "Invitational Team"
   mutate(uci_division_name = replace_na(uci_division_name,'Invitational Team'))
 }
+
