f956ad23a87516f23601b07b11c020fe7605c856
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2024-02-07 20:34:27 +0000

    Update Team_Details_extract.py
    added commentary

--- backend_team_details/Team_Details_extract.py
+++ backend_team_details/Team_Details_extract.py
@@ -14,27 +14,38 @@ from random import randrange
 
 ##### Making Code Possible to Put onto Github #####
 
-setwd = '/Users/zacrogers/Documents/cycling_chaos/python_code/'
+# setwd = working directory
 
+# set season you want to ingest
 season = 2023
+# fc_division_id starts at 1
 fc_division_count = 1
+# create lists of preingested data to append to during the ingestion process
 cci_output = pd.read_csv(setwd+'cycling_chaos_ingestion_df_master.csv')['output'].to_list()
 cci_output_details = pd.read_csv(setwd+'cycling_chaos_ingestion_df_master.csv')['output_details'].to_list()
 cci_file_name = pd.read_csv(setwd+'cycling_chaos_ingestion_df_master.csv')['file_name'].to_list()
 
-for fc_division_count in tqdm(range(1,
-                                          10
+# ingestion loop
+
+# there are 10 divisions within first_cycling
+for fc_division_count in tqdm(range(0,
+                                          9
                                         )):
+    # get code to sleep for 5 seconds to not overload website.
+    # probably should look at making this dynamic and more random
     time.sleep(5)
+    # url = base website + season + '&d=' + division_id. 'd=' means division
     url = 'https://firstcycling.com/team.php?y='+str(season)+'&d='+str(fc_division_count)
+    # beautiful soup to ingest html file
     teamdetails_meta = requests.get(url)
     teamdetails_meta_soup = BeautifulSoup(teamdetails_meta.content, "html.parser")
     teamdetails_meta_soup_str = str(teamdetails_meta_soup)
+    # create file_name and write to disk
     file_name = 'cycling_chaos_code'+'_'+'team_details'+'_'+'all'+'_'+str(season)+'_'+str(fc_division_count)+'.txt'
     with open(setwd+'calendar_ingestion_files/souped_html_txt_files/'+file_name, 'w') as writefile:
-    # with open(r'/Users/zacrogers/Documents/cycling_chaos/python_code/calendar_ingestion_files/souped_html_txt_files/'+file_name, 'w') as writefile:
         writefile.write(teamdetails_meta_soup_str)
         writefile.close()
+    # append ingestion tracker lists, convert to dataframe and write to disk
     cci_output.append('team_details')
     cci_output_details.append('all')
     cci_file_name.append(file_name)
