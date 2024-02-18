24a2a56bb1f824995120a20e43c273691a977cbe
Zac Rogers <89270891+rogers1000@users.noreply.github.com>
2024-02-18 13:19:00 +0000

    Update Team_Details_extract.py
    updated so it works over multiple seasons as well as works historically.

--- backend_team_details/Team_Details_extract.py
+++ backend_team_details/Team_Details_extract.py
@@ -28,31 +28,35 @@ cci_file_name = pd.read_csv(setwd+'cycling_chaos_ingestion_df_master.csv')['file
 # ingestion loop
 
 # there are 10 divisions within first_cycling
-for fc_division_count in tqdm(range(0,
-                                          9
+for season_count in tqdm(range(0,
+                                          2
                                         )):
-    # get code to sleep for 5 seconds to not overload website.
-    # probably should look at making this dynamic and more random
-    time.sleep(5)
-    # url = base website + season + '&d=' + division_id. 'd=' means division
-    url = 'https://firstcycling.com/team.php?y='+str(season)+'&d='+str(fc_division_count)
-    # beautiful soup to ingest html file
-    teamdetails_meta = requests.get(url)
-    teamdetails_meta_soup = BeautifulSoup(teamdetails_meta.content, "html.parser")
-    teamdetails_meta_soup_str = str(teamdetails_meta_soup)
-    # create file_name and write to disk
-    file_name = 'cycling_chaos_code'+'_'+'team_details'+'_'+'all'+'_'+str(season)+'_'+str(fc_division_count)+'.txt'
-    with open(setwd+'calendar_ingestion_files/souped_html_txt_files/'+file_name, 'w') as writefile:
-        writefile.write(teamdetails_meta_soup_str)
-        writefile.close()
-    # append ingestion tracker lists, convert to dataframe and write to disk
-    cci_output.append('team_details')
-    cci_output_details.append('all')
-    cci_file_name.append(file_name)
+    for fc_division_count in range(1,10):
+        # get code to sleep for 5 seconds to not overload website.
+        # probably should look at making this dynamic and more random
+        time.sleep(5)
+        # url = base website + season + '&d=' + division_id. 'd=' means division
+        url = 'https://firstcycling.com/team.php?y='+str(season)+'&d='+str(fc_division_count)
+        # beautiful soup to ingest html file
+        teamdetails_meta = requests.get(url)
+        teamdetails_meta_soup = BeautifulSoup(teamdetails_meta.content, "html.parser")
+        teamdetails_meta_soup_str = str(teamdetails_meta_soup)
+        # create file_name and write to disk
+        file_name = 'cycling_chaos_code'+'_'+'team_details'+'_'+'all'+'_'+str(season)+'_'+str(fc_division_count)+'.txt'
+        with open(setwd+'souped_html_txt_files/'+file_name, 'w') as writefile:
+            writefile.write(teamdetails_meta_soup_str)
+            writefile.close()
+        # append ingestion tracker lists, convert to dataframe and write to disk
+        cci_output.append('team_details')
+        cci_output_details.append('all')
+        cci_file_name.append(file_name)
 
-    print('Ingested #'+str(fc_division_count+1)+' '+file_name)
+        print('Ingested #'+str(fc_division_count+1)+' '+file_name)
 
-    fc_division_count = fc_division_count + 1
+        fc_division_count = fc_division_count + 1
+
+    season = season + 1
+season_count = season_count + 1
 
 cycling_chaos_ingestion = pd.DataFrame({'output':cci_output, 'output_details':cci_output_details, 'file_name':cci_file_name})
 
