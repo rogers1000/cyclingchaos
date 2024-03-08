2cf272155743de1b65fc1cdb17b016630868344e
Rogers <89270891+rogers1000@users.noreply.github.com>
2024-03-08 19:10:22 +0000

    Update AussieWT_output.R

--- Example_Work/Race_Block/AussieWT/AussieWT_output.R
+++ Example_Work/Race_Block/AussieWT/AussieWT_output.R
@@ -1,1 +1,15 @@
+# Men's Start List
 
+print(startlist_heatmap(2024,'men','Aussie WT'))
+
+# Women's Start List 
+
+print(startlist_heatmap(2024,'women','Aussie WT'))
+
+# Men's Combined Races GC 
+
+print(results_pivot(2024,"men","Rider","GC6","Aussie WT","","","",1,77,"CyclingChaos.co.uk"))
+
+# Women's Combined Races GC
+
+print(results_pivot(2024,"women","Rider","GC6","Aussie WT","","","",1,77,"CyclingChaos.co.uk"))
