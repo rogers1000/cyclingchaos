65cc687470a6d3798371d8ef6dce514f01805ad0
Rogers <89270891+rogers1000@users.noreply.github.com>
2024-03-09 11:56:29 +0000

    Update calendar_backend_function_r.r
    added PN-TA Week to function

--- backend_race_calendar/calendar_backend_function_r.r
+++ backend_race_calendar/calendar_backend_function_r.r
@@ -1,5 +1,5 @@
 library(tidyverse)
-# no pre-built filters
+
 calendar_function <- function() {
   # load csv from transformation using python
   #calendar_csv <- read.csv('https://raw.githubusercontent.com/rogers1000/cyclingchaos/main/backend_race_calendar/CyclingChaos_RaceCalendar_df_master.csv') |>
@@ -22,7 +22,7 @@ calendar_function <- function() {
                                      first_cycling_race_id == "24" ~ "Big7",
                                      first_cycling_race_id == "26" ~ "Big7",
                                      first_cycling_race_id == "54" ~ "Big7",
-                                         .default = "")) |>
+                                     .default = "")) |>
     mutate(race_tag_grandtour = case_when(first_cycling_race_id == "13" ~ "Grand Tour",
                                           first_cycling_race_id == "17" ~ "Grand Tour",
                                           first_cycling_race_id == "23" ~ "Grand Tour",
@@ -40,41 +40,48 @@ calendar_function <- function() {
                                                 first_cycling_race_id == "5" ~ "Cobbled Classic",
                                                 .default = "")) |>
     mutate(race_tag_ardennes = case_when(first_cycling_race_id == "9" ~ "Ardennes",
-                                          first_cycling_race_id == "10" ~ "Ardennes",
-                                          first_cycling_race_id == "11" ~ "Ardennes",
-                                          .default = "")) |>
-    mutate(race_tag_cobbles_openingweekend = case_when(first_cycling_race_id == "53" ~ "Cobbles Opening Weekend",
-                                         first_cycling_race_id == "84" ~ "Cobbles Opening Weekend",
+                                         first_cycling_race_id == "10" ~ "Ardennes",
+                                         first_cycling_race_id == "11" ~ "Ardennes",
                                          .default = "")) |>
+    mutate(race_tag_cobbles_openingweekend = case_when(first_cycling_race_id == "53" ~ "Cobbles Opening Weekend",
+                                                       first_cycling_race_id == "84" ~ "Cobbles Opening Weekend",
+                                                       first_cycling_race_id == "9201" ~ "Cobbles Opening Weekend",
+                                                       first_cycling_race_id == "9181" ~ "Cobbles Opening Weekend",
+                                                       .default = "")) |>
     mutate(race_tag_aussie_wt = case_when(first_cycling_race_id == "1" ~ "Aussie WT",
-                                               first_cycling_race_id == "1172" ~ "Aussie WT",
-                                               first_cycling_race_id == "9080" ~ "Aussie WT",
-                                               first_cycling_race_id == "9133" ~ "Aussie WT",
-                                               .default = "")) |>
+                                          first_cycling_race_id == "1172" ~ "Aussie WT",
+                                          first_cycling_race_id == "9080" ~ "Aussie WT",
+                                          first_cycling_race_id == "9133" ~ "Aussie WT",
+                                          .default = "")) |>
     
     mutate(race_tag_middle_east = case_when(first_cycling_race_id == "868" ~ "Middle East Races",
                                             #first_cycling_race_id == "57" ~ "Middle East Races",
                                             first_cycling_race_id == "9800" ~ "Middle East Races",
-                                               .default = "")) |>
+                                            .default = "")) |>
     mutate(race_tag_world_champs = case_when(first_cycling_race_id == "26" ~ "World Champs",
-                                               first_cycling_race_id == "27" ~ "World Champs",
-                                               .default = "")) |>
-    mutate(race_tag_euro_champs = case_when(first_cycling_race_id == "4020" ~ "Euro Champs",
-                                             first_cycling_race_id == "4019" ~ "Euro Champs",
+                                             first_cycling_race_id == "27" ~ "World Champs",
                                              .default = "")) |>
+    mutate(race_tag_euro_champs = case_when(first_cycling_race_id == "4020" ~ "Euro Champs",
+                                            first_cycling_race_id == "4019" ~ "Euro Champs",
+                                            .default = "")) |>
     mutate(race_tag_challenge_mallorca = case_when(first_cycling_race_id == "100" ~ "Challenge Mallorca",
-                                          first_cycling_race_id == "271" ~ "Challenge Mallorca",
-                                          first_cycling_race_id == "274" ~ "Challenge Mallorca",
-                                          first_cycling_race_id == "83" ~ "Challenge Mallorca",
-                                          .default = "")) |>
+                                                   first_cycling_race_id == "271" ~ "Challenge Mallorca",
+                                                   first_cycling_race_id == "274" ~ "Challenge Mallorca",
+                                                   first_cycling_race_id == "83" ~ "Challenge Mallorca",
+                                                   .default = "")) |>
+    mutate(race_tag_pn_ta_week = case_when(first_cycling_race_id == "3" ~ "PN-TA Week",
+                                            first_cycling_race_id == "2" ~ "PN-TA Week",
+                                            .default = "")) |>
+    mutate(test = case_when(first_cycling_race_id == "9133" ~ "Test",
+                            .default = "")) |>
     # race_tags column = all race blocks within one column
     mutate(race_tags = paste(race_tag_monument,race_tag_world_tour,race_tag_big7,race_tag_grandtour,race_tag_cobbled_classic,
                              race_tag_ardennes,race_tag_cobbles_openingweekend,race_tag_aussie_wt,race_tag_middle_east,race_tag_world_champs,
-                             race_tag_euro_champs,race_tag_challenge_mallorca,sep = " ")) |>
+                             race_tag_euro_champs,race_tag_challenge_mallorca,race_tag_pn_ta_week,test,sep = " ")) |>
     # hide race tags column creation columns
     select(-c(race_tag_monument,race_tag_world_tour,race_tag_big7,race_tag_grandtour,race_tag_cobbled_classic,
               race_tag_ardennes,race_tag_cobbles_openingweekend,race_tag_challenge_mallorca,race_tag_middle_east,race_tag_world_champs,
-              race_tag_euro_champs,race_tag_aussie_wt)) |>
+              race_tag_euro_champs,race_tag_aussie_wt,race_tag_pn_ta_week,test)) |>
     # make race_id as double
     mutate(first_cycling_race_id = as.double(first_cycling_race_id))
 }
