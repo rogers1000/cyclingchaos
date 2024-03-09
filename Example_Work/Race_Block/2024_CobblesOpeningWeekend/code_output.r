78b43a8ee32383f061b8cc289ba0f85b7ea43491
Rogers <89270891+rogers1000@users.noreply.github.com>
2024-03-09 10:11:36 +0000

    Create code_output.r

---
+++ Example_Work/Race_Block/2024_CobblesOpeningWeekend/code_output.r
@@ -0,0 +1,16 @@
+# Women’s start list heat map
+
+print(startlist_heatmap(2024,’women’,'Cobbles Opening Weekend'))
+
+# Women's Combined Races GC
+
+print(results_pivot(2024,”women”,"Rider","GC6","Cobbles Opening Weekend","","","",1,20,"CyclingChaos.co.uk"))
+
+
+# Men’s start list heat map
+
+print(startlist_heatmap(2024,’men','Cobbles Opening Weekend'))
+
+# Men's Combined Races GC
+
+print(results_pivot(2024,"men","Rider","GC6","Cobbles Opening Weekend","","","",1,20,"CyclingChaos.co.uk"))
